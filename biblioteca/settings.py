1efc7b3c7b4dbef8dd3a977ef9fd6797834e67af
jjosinoms@gmail.com <jjosinoms@gmail.com>
2018-03-23 11:54:13 -0300

    Atualizacao com ajuda do Gileno

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -129,3 +129,20 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_URL = 'logout'
 LOGOUT_REDIRECT_URL = 'home'
+
+
+#Heroku settings
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is)secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# ALLOW all hosts headers
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR,' static'),)
